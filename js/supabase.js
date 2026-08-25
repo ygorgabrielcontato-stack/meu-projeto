b79f42f7ce469a089d271b66369c7a1ff6c0917c
ygorgabrielcontato-stack <ygorgabrielcontato@gmail.com>
2026-08-25 18:02:10 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,7 @@ const SUPABASE_URL = "https://rripowawxwqmxvooeanr.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_qA7SZbY3_rqwX_GnJ3oKMg_afLM7-RV";
 
-const supabase = window.supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
 );
